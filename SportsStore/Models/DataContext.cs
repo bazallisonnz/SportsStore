d006d09acf97acd0edc464c3e538dd4377cc9009
Barry Allison <bazallison@hotmail.com>
2018-04-28 17:05:46 +0200

    Implemented using EF Core 2. Migration created and Database created.

---
+++ SportsStore/Models/DataContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SportsStore.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> opts) : base(opts) { }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ SportsStore/Models/DataRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace SportsStore.Models
+{
+    public class DataRepository : IRepository
+    {
+        private DataContext context;
+
+        public DataRepository(DataContext ctx) => context = ctx;
+
+        public IEnumerable<Product> Products => context.Products;
+
+        public void AddProduct(Product product)
+        {
+            this.context.Products.Add(product);
+            this.context.SaveChanges();
+        }
+    }
+}
